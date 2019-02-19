2ed16fe8850915127c72464896ea60a094eb5580
R453L <rrss.mahmud@gmail.com>
2019-02-20 01:02:06 +0530

    Initial commit

---
+++ app/src/main/java/com/example/bms/myapplication/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.bms.myapplication;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button b1,b2;
+        b1 = findViewById(R.id.button1);
+        b2 = findViewById(R.id.button2);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.framelayout,new Fragment1());
+                ft.commit();
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.framelayout,new Fragment2());
+                ft.commit();
+            }
+        });
+    }
+}
